2194b22708044ab38ae2573097e3d25f9e6ee064
Liiii <838729769@qq.com>
2017-12-21 09:34:56 +0800

    Update P1996.cpp and fixed bug....
    emmm...

--- P1996.cpp
+++ P1996.cpp
@@ -1,45 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct list{
-	int data;
-	int next;
-	
-};
+struct list {
+  int data;
+  int next;
+}; //生成节点
 
 list round[101];
 
-void round_complete(int n){
-	for(int i = 1; i <= n; i++){
-		round[i].data = i;
-		
-		if (i == n) {
-			round[i].next = 1;
-			break;
-		}
-		else{
-			round[i].next = i+1;	
-		}
-		
-	}
+void round_complete(int n) {
+  for (int i = 1; i <= n; i++) {
+    round[i].data = i;
+
+    if (i == n) {
+      round[i].next = 1;
+      break;
+    } else {
+      round[i].next = i + 1;
+    }
+  }
 }
 
-int main(){
-	int n, m;
-	cin >> n >> m;
-	round_complete(n);
-	while(n){
-		int p, q;
-		for(int i = 1; i <= m; i++){
-			p = round[p].next;
-			q = 
-		}//拿到出圈人的编号
-		
-		
-		
-	}
-		
-	
-	
-	return 0;
+int main() {
+  int n, m;
+  cin >> n >> m;
+  round_complete(n);
+
+  int p = round[n].data;
+  while (n > 0) {
+    if (n == 1) {
+      cout << round[p].data;
+      break;
+    }
+
+    for (int i = 1; i < m; i++) {
+      p = round[round[p].next].data;
+    } //拿到出圈人的编号
+    cout << round[p].next << " ";
+    round[p].next = round[round[p].next].next;
+    n--;
+  }
+
+  return 0;
 }
