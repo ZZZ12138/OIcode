d9cc545ceffdd84edb9f9d0a0d02c5a62c5670f8
Liiii <838729769@qq.com>
2017-12-28 09:35:29 +0800

    Update P1090.cpp

--- P1090.cpp
+++ P1090.cpp
@@ -1,37 +1,36 @@
-#include<iostream>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-int fruit[10000+10];
+int fruit[10000 + 10];
 
-void _swap(int &a, int &b){
-	int temp = a;
-	a = b;
-	b = temp;
+void _swap(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
 }
 
-int main(){
-	int n;
-	cin >> n;
-	
-	for (int i = 1; i <= n; i++){
-		cin >> fruit[i];		
-	}
-	sort(fruit, fruit+n+1);
-	
-	
-	int tot = 0;
-	for (int i = 2; i <= n; i++){
-		fruit[i] = fruit[i] + fruit[i-1];
-		tot += fruit[i]; 
-//		cout << fruit[i] << endl;
-		int m = i;
-		while(fruit[m+1] < fruit[m] || m < n){
-			_swap(fruit[m+1], fruit[m]);
-			m++;
-		}//用冒泡排序将新的果子堆扔到合适的位置（冒泡排序的思想） 
-		
-	}
-	cout << tot;
-	return 0;
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    cin >> fruit[i];
+  }
+  sort(fruit, fruit + n + 1);
+
+  int tot = 0;
+  for (int i = 2; i <= n; i++) {
+    fruit[i] = fruit[i] + fruit[i - 1];
+    tot += fruit[i];
+    //		cout << fruit[i] << endl;
+    int m = i;
+    while (fruit[m + 1] < fruit[m] && m < n) {
+      
+      _swap(fruit[m + 1], fruit[m]);
+      m++;
+    } //用冒泡排序将新的果子堆扔到合适的位置（冒泡排序的思想）
+  }
+  cout << tot;
+  return 0;
 }
