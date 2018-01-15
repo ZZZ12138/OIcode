012e3c979296363142dcedde72b492f50e9a3fd0
Liiii <838729769@qq.com>
2018-01-15 14:22:15 +0800

    Update P1309.cpp
    需要归并排序

--- P1309.cpp
+++ P1309.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct person{
@@ -14,6 +15,39 @@ bool cmp1(person a, person b){
     else return a.mark > b.mark;
 }
 
+void _swap(person &a, person &b){
+	struct person temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void complete(int point){
+	while(point > 1){
+		if(player[point-1].mark < player[point].mark){
+		    _swap(player[point-1],player[point]);//如果分数低，往前排
+			point--;
+			continue;
+			
+		}
+		else{
+	 	    if(player[point-1].mark == player[point].mark){
+	 	    	if(player[point-1].id > player[point].id){
+	 	    		_swap(player[point-1],player[point]);
+	 	    		point--;
+	 	    	}
+	 	    	else{
+	 	    		break;
+	 	    	}
+		    }
+		    else{
+		    	break;
+		    }
+	    }	 
+    }
+	return; 
+} 
+
 int main(){
     int n;
     int r;
@@ -26,10 +60,22 @@ int main(){
     for(int i = 1; i <= 2*n; i++){
         cin >> player[i].power;
     }
-    sort(player+1,player+1+2*n,cmp1);[p;/'']
-    for(int i = 1; i <= 2*n; i += 2){
-
-    }
-
-
+ 
+    sort(player+1,player+1+2*n,cmp1);
+    for(int j = 1; j <= r; j++){//进行r轮比赛 
+		for(int i = 1; i <= 2*n; i += 2){
+    		if(player[i].power > player[i+1].power){
+    			player[i].mark++;
+    			//排序
+    			complete(i);
+    		}
+    		else{
+    			 player[i+1].mark++;
+    			 //排序
+    			 complete(i+1);   
+    		} 
+		}
+    } 
+    cout << player[q].id;
+    return 0;
 }
