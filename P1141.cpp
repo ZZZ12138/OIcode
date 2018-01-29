3d30edeb1faea4eae75cc37b75d4ea9c8a744a59
Liiii <838729769@qq.com>
2018-01-29 14:19:11 +0800

    Update P1141.cpp

--- P1141.cpp
+++ P1141.cpp
@@ -33,28 +33,26 @@ void dfs(int x, int y) {
 	if (x < 1 || x > n || y < 1 || y > n) {
 		return;//超出边界
 	}
-	if (temp[x][y] != 0) return;
-	else {
-		tot++;
-		int temp1 = block[x][y];
-		if (block[x - 1][y] != temp1) {
-			temp[x - 1][y] = 1;
-			dfs(x - 1, y);
-		}
-		if (block[x + 1][y] != temp1) {
-			temp[x + 1][y] = 1;
-			dfs(x + 1, y);
-		}
-		if (block[x][y - 1] != temp1) {
-			temp[x][y - 1] = 1;
-			dfs(x, y - 1);
-		}
-		if (block[x][y + 1] != temp1) {
-			temp[x][y + 1] = 1;
-			dfs(x, y + 1);
-		}
-		ans[x][y] = tot;
+	if (temp[x][y] != 0) {
+		return;//被搜索过
 	}
+
+	tot++;
+	temp[x][y] = 1;
+	int temp1 = block[x][y];
+	if (block[x - 1][y] != temp1) {
+		dfs(x - 1, y);
+	}
+	if (block[x + 1][y] != temp1) {
+		dfs(x + 1, y);
+	}
+	if (block[x][y - 1] != temp1) {
+		dfs(x, y - 1);
+	}
+	if (block[x][y + 1] != temp1) {
+		dfs(x, y + 1);
+	}
+	ans[x][y] = tot;
 }
 
 int main() {
@@ -62,15 +60,14 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int x, y;
 		cin >> x >> y;
-		if (ans[x][y] != 0) cout << ans[x][y];
+		if (ans[x][y] != 0) {
+			cout << ans[x][y] << endl;
+		}
 		else {
 			tot = 0;
 			dfs(x, y);
-			cout << ans[x][y];
+			cout << ans[x][y] << endl;
 		}
-
 	}
-
-
 	return 0;
 }
