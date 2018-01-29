cb49244e1658d5c71daf50c60de9e181ebbe4b07
Liiii <838729769@qq.com>
2018-01-29 12:48:17 +0800

    Create P1141.cpp
    Not complete

---
+++ P1141.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int temp[1000 + 10][1000 + 10];
+int block[1000 + 10][1000 + 10];
+int ans[1000 + 10][1000 + 10];
+
+struct point {
+	int x;
+	int y;
+};
+
+
+int n, m;
+
+void in() {
+	cin >> n >> m;
+	string temp1;
+	for (int i = 1; i <= n; i++) {
+		cin >> temp1;
+		for (int j = 1; j <= n; j++)
+		{
+			block[i][j] = temp1[j - 1] - 48;
+		}
+	}
+}
+
+int tot;
+
+void dfs(int x, int y) {
+	if (x < 1 || x > n || y < 1 || y > n) {
+		return;//超出边界
+	}
+	if (temp[x][y] != 0) return;
+	else {
+		tot++;
+		int temp1 = block[x][y];
+		if (block[x - 1][y] != temp1) {
+			temp[x - 1][y] = 1;
+			dfs(x - 1, y);
+		}
+		if (block[x + 1][y] != temp1) {
+			temp[x + 1][y] = 1;
+			dfs(x + 1, y);
+		}
+		if (block[x][y - 1] != temp1) {
+			temp[x][y - 1] = 1;
+			dfs(x, y - 1);
+		}
+		if (block[x][y + 1] != temp1) {
+			temp[x][y + 1] = 1;
+			dfs(x, y + 1);
+		}
+		ans[x][y] = tot;
+	}
+}
+
+int main() {
+	in();
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+		if (ans[x][y] != 0) cout << ans[x][y];
+		else {
+			tot = 0;
+			dfs(x, y);
+			cout << ans[x][y];
+		}
+
+	}
+
+
+	return 0;
+}
