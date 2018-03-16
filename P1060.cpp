7b4ba03296052f01c240927d8ecdd4b797f213b0
Liiii <838729769@qq.com>
2018-03-16 14:20:16 +0800

    Create P1060.cpp
    Not complete

---
+++ P1060.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+struct it{
+	int value;
+	int weight;
+};
+
+it item[10000];
+
+int main(){
+	int n, m;
+	cin >> n >> m;
+	for(int i = 1; i <= m; i++){
+		cin >> item.value >> item.weight;
+	}
+	
+	
+	
+}
